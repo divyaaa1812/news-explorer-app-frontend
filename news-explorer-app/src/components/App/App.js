2d81d56a9f7d0c7f39b57197dc79b5c6a2130469
Divya B <divyaaa1812@gmail.com>
2024-02-05 18:43:26 -0800

    changes

--- news-explorer-app/src/components/App/App.js
+++ news-explorer-app/src/components/App/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -18,6 +18,10 @@ function App() {
           Learn React
         </a>
       </header>
+      <Switch>
+        <Route path="/"></Route>
+        <Route path=""></Route>
+      </Switch>
     </div>
   );
 }
