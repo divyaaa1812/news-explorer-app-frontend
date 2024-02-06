5234867052b2e4ff9725deb9b0b3348871ef0cac
Divya B <divyaaa1812@gmail.com>
2024-02-05 22:01:16 -0800

    changes

--- news-explorer-app/src/components/App/App.js
+++ news-explorer-app/src/components/App/App.js
@@ -1,12 +1,23 @@
 // App.js
 import React from "react";
 import Header from "../Header/Header";
+import Main from "../Main/Main";
+import SavedNews from "../SavedNews/SavedNews";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route exact path="/">
+          <Main />
+        </Route>
+        <Route exact path="/saved-news">
+          <SavedNews />
+        </Route>
+      </Routes>
     </div>
   );
 }
