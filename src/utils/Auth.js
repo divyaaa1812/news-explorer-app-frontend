c61c80c85cec71320c35180a596205ac235af3f5
Divya B <divyaaa1812@gmail.com>
2024-04-01 11:26:50 -0700

    auth file changes

--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -1,15 +1,11 @@
 import { request, baseUrl } from "../utils/MainApi";
 
 export const verifyToken = (token) => {
-  return fetch(`${baseUrl}/users/me`, {
+  return request(`${baseUrl}/users/me`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
       authorization: `Bearer ${token}`,
     },
-  }).then((res) => {
-    if (res.ok) {
-      return res.json();
-    }
   });
 };
